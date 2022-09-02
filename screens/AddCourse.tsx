27a524ae3adff19a8508b1fcc62855030cdd9df0
Murtaza Pakawala <mypaka52@gmail.com>
2022-09-03 02:15:16 +1000

    adding touchable and also handling basic logic for submit

--- screens/AddCourse.tsx
+++ screens/AddCourse.tsx
@@ -1,4 +1,11 @@
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import {
+  Alert,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React, { useState } from "react";
 import { Menu, Provider, Surface, TextInput } from "react-native-paper";
 import DropDown from "react-native-paper-dropdown";
@@ -6,6 +13,8 @@ const AddCourse = () => {
   //states
   const [showDropDown, setShowDropDown] = useState(false);
   const [term, setTerm] = useState("");
+  const [courseCode, setCourseCode] = useState("");
+  const [courseName, setCourseName] = useState("");
   const termList = [
     {
       label: "T1",
@@ -20,6 +29,15 @@ const AddCourse = () => {
       value: "T3",
     },
   ];
+  // function after button add course
+  const onPress = () => {
+    console.log(term);
+    console.log(courseCode);
+    console.log(courseName);
+    Alert.alert(
+      `course code ${courseCode} course name ${courseName} course term ${term}`
+    );
+  };
   return (
     <Provider>
       <SafeAreaView style={styles.container}>
@@ -28,16 +46,18 @@ const AddCourse = () => {
           style={styles.textInput}
           mode="outlined"
           maxLength={8}
+          onChangeText={(text) => setCourseCode(text)}
           placeholder="Enter Course Name"
           right={<TextInput.Affix text="/8" />}
         />
         <Text style={styles.title}>Enter Course Name</Text>
         <TextInput
+          onChangeText={(text) => setCourseName(text)}
           style={styles.textInput}
           mode="outlined"
           placeholder="Enter Course Name"
         />
-        <View style={{ margin: 10 }}>
+        <View style={{ width: 350, margin: 10 }}>
           <DropDown
             dropDownStyle={{ marginTop: -34 }}
             label={"Term"}
@@ -50,6 +70,11 @@ const AddCourse = () => {
             list={termList}
           />
         </View>
+        <TouchableOpacity style={styles.button} onPress={onPress}>
+          <Text style={{ color: "black", fontSize: 20, fontWeight: "bold" }}>
+            Add Course
+          </Text>
+        </TouchableOpacity>
       </SafeAreaView>
     </Provider>
   );
@@ -58,7 +83,9 @@ export default AddCourse;
 
 const styles = StyleSheet.create({
   container: {
+    display: "flex",
     padding: 20,
+    alignItems: "center",
   },
   title: {
     margin: 10,
@@ -68,5 +95,14 @@ const styles = StyleSheet.create({
   },
   textInput: {
     margin: 10,
+    width: 350,
+  },
+  button: {
+    margin: 10,
+    backgroundColor: "white",
+    width: 150,
+    padding: 10,
+    alignItems: "center",
+    borderRadius: 20,
   },
 });
